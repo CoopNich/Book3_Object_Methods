582f0cb59708b998340d5a96dc1ce8d8d5ed4b16
Cooper Nichols <coopnich92@gmail.com>
2020-02-11 08:55:50 -0600

    lightning exercises complete

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,15 +6,15 @@ const dateVisited = "visitDate"
 const owed = "amountBilled"
 const patient = "patientName"
 
-const billFromDoctor = 
-    {
-        officeName: "Pete's Practice",
-        streetAddress: "123 Main",
-        doctorName: "Dr. Pete",
-        patientName: "Jim",
-        visitDate: "October 12",
-        amountBilled: 50.00
-    }
+const billFromDoctor =
+{
+    officeName: "Pete's Practice",
+    streetAddress: "123 Main",
+    doctorName: "Dr. Pete",
+    patientName: "Jim",
+    visitDate: "October 12",
+    amountBilled: 50.00
+}
 
 
 console.log(billFromDoctor[dateVisited])
@@ -36,7 +36,23 @@ for (const value of Object.keys(billFromDoctor)) {
 const infoContainer = document.querySelector("#app")
 
 for (const value of Object.keys(billFromDoctor)) {
-  infoContainer.innerHTML += `<div>${value}</div>`
+    infoContainer.innerHTML += `<div>${value}</div>`
+
+}
+
+console.log("-------------")
+
+pizzaContainer = document.querySelector(".food")
+
+const yummyPizza = {
+    type: "thin crust",
+    cheese: true,
+    protein: "pepperoni",
+    vegetable: "onion"
+}
+pizzaContainer.innerHTML += `<h5>Yummy Pizza</h5>`
+for (const entry of Object.entries(yummyPizza)) {
+    pizzaContainer.innerHTML += `<div>${entry[0]}:${entry[1]}</div>`
 }
 
 
