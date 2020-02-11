a0050e53b566b57e0bff0630357973851066daab
Cooper Nichols <coopnich92@gmail.com>
2020-02-10 21:43:37 -0600

    more lightning exercises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,48 +1,6 @@
-const allCars = [
-    {
-        model: "Mustang",
-        color: "Midnight Blue",
-        year: 1976,
-        length: 120,
-        width: 62,
-        height: 47
-    },
-    {
-        model: "Xterra",
-        color: "Forest Green",
-        year: 2011,
-        length: 144,
-        width: 71,
-        height: 55
-    },
-    {
-        model: "Thunderbird",
-        color: "Red",
-        year: 2005,
-        length: 115,
-        width: 58,
-        height: 42
-    },
-    {
-        model: "Suburban",
-        color: "Grey",
-        year: 2015,
-        length: 149,
-        width: 73,
-        height: 58
-    }
-]
 
 
-const outputElement = document.querySelector("#app")
-
-allCars.forEach(car => {
-    for (const value of Object.values(car)) {
-        outputElement.innerHTML += `<div>${value}</div>`
-      }
-})
-
-// Lightning Exercise 1 For Of with Object.values()
+// Lightning Exercises For Of with Object.values()
 
 const dateVisited = "visitDate"
 const owed = "amountBilled"
@@ -68,5 +26,18 @@ console.log("--------------")
 for (const value of Object.values(billFromDoctor)) {
     console.log(value)
 }
+
+console.log("--------------")
+
+for (const value of Object.keys(billFromDoctor)) {
+    console.log(value)
+}
+
+const infoContainer = document.querySelector("#app")
+
+for (const value of Object.keys(billFromDoctor)) {
+  infoContainer.innerHTML += `<div>${value}</div>`
+}
+
 
 
